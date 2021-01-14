67ce5b0bd72094f8fac6dc08757dc1621d09bd77
Pragyansh Dwivedi <pragyansh99@gmail.com>
2021-01-14 23:43:30 +0530

    Heroku configured

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rental-department'));
+app.use(express.static(__dirname + '/dist/angular-mini-social-network'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/rental-department/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-mini-social-network/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
